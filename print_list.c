05c830ba7531261a9318ddf35115a601c1c026ad
zokeya <zdokeya@gmail.com>
2023-06-24 14:07:26 +0300

    Mandatory Tasks

--- print_list.c
+++ print_list.c
@@ -5,21 +5,19 @@
  * print_list - Prints a list of integers
  *
  * @list: The list to be printed
-*/
+ */
 void print_list(const listint_t *list)
 {
-  int i;
+    int i;
 
-  i=0;
-  while (list)
-  {
-    if (i > 0)
+    i = 0;
+    while (list)
     {
-      printf(", ");
+        if (i > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++i;
+        list = list->next;
     }
-    printf("%d", list->n);
-    ++i;
-    list = list->next;
-  }
-  printf("\n");
+    printf("\n");
 }

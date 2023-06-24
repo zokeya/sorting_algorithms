78bceb087863b9a231691ec18b8017ddca249ca1
zokeya <zdokeya@gmail.com>
2023-06-24 11:43:30 +0300

    Mandatory tasks

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "sort.h"
+
+void swap_ints(int *a, int *b)
+{
+  int tmp;
+
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order
+ * @array: An array of integers to be sorted
+ * @size: The size of array
+ *
+ * Description: Prints the array after each swap
+*/
+void bubble_sort(int *array, size_t size)
+{
+  size_t i, len = size;
+  bool bubbly = false;
+
+  if (array == NULL || size < 2)
+  {
+    return;
+  }
+
+  while (bubbly == false)
+  {
+    bubbly = true;
+    for (i = 0; i < len; i++)
+    {
+      if (array[i] > array[i + 1])
+      {
+        swap_ints(array + 1, array + i + 1);
+        print_array(array, size);
+        bubbly == false;
+      }
+    }
+    len--;
+  }
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swaps two integers in an array
+ * @a: The first integer to swap
+ * @b: the second integer to swap
+*/
+void swap_ints(int *a, int *b)
+{
+  int tmp;
+
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+/**
+ * selection_sort - Sorts an away of integers in ascending order
+ *   using the lection sort algorithm
+ * @array: An awwy of integers
+ * @size: The size of the array
+ *
+ * Description: Prints the array ofater each swap
+*/
+void selection_sort(int *array, size_t size)
+{
+  int *min;
+  size_t j, k;
+
+  if (array == NULL ||size < 2)
+  {
+    return;
+  }
+
+  for (j = 0; j < size - 1; j++)
+  {
+    min = array + j;
+    for (k = j + 1; k < size; k++)
+    {
+      min = (array[k] < *min) ? (array + k) : min;
+    }
+
+    if ((array + j) != min)
+    {
+      swap_ints(array + j, min);
+      print_array(array, size);
+    }
+  }
+}

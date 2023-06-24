197278660a2e4d776c44c11b0b34599619ffc881
zokeya <zdokeya@gmail.com>
2023-06-24 06:18:31 -0700

    Mandatory Tasks 24-06-2023

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,11 +12,11 @@ void quick_sort(int *array, size_t size);
 */
 void swap_ints(int *a, int *b)
 {
-  int tmp;
+	int tmp;
 
-  tmp = *a;
-  *a = *b;
-  *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -31,29 +31,27 @@ void swap_ints(int *a, int *b)
 */
 int lomuto_partition(int *array, size_t size, int left, int right)
 {
-  int *driver, up, down;
+	int *driver, up, down;
 
-  driver =  array + right;
-  for (up = down = left; down < right; down++)
-  {
-    if (array[down] < *driver)
-    {
-      if (up < down)
-      {
-        swap_ints(array + down, array + up);
-        print_array(array, size);
-      }
-      up++;
-    }
-  }
-
-  if (array[up] > *driver)
-  {
-    swap_ints(array + up, driver);
-    print_array(array, size);
-  }
-
-  return (up);
+	driver =  array + right;
+	for (up = down = left; down < right; down++)
+	{
+		if (array[down] < *driver)
+		{
+			if (up < down)
+			{
+				swap_ints(array + down, array + up);
+				print_array(array, size);
+			}
+			up++;
+		}
+	}
+	if (array[up] > *driver)
+	{
+		swap_ints(array + up, driver);
+		print_array(array, size);
+	}
+	return (up);
 }
 
 /**
@@ -67,14 +65,14 @@ int lomuto_partition(int *array, size_t size, int left, int right)
 */
 void lomuto_sort(int *array, size_t size, int left, int right)
 {
-  int belong;
+	int belong;
 
-  if (right - left > 0)
-  {
-    belong = lomuto_partition(array, size, left, right);
-    lomuto_sort(array, size, left, belong - 1);
-    lomuto_sort(array, size, belong + 1, right);
-  }
+	if (right - left > 0)
+	{
+		belong = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, belong - 1);
+		lomuto_sort(array, size, belong + 1, right);
+	}
 }
 
 /**
@@ -88,11 +86,7 @@ void lomuto_sort(int *array, size_t size, int left, int right)
 */
 void quick_sort(int *array, size_t size)
 {
-  if (array == NULL || size < 2)
-  {
-    return;
-  }
-
-  lomuto_sort(array, size, 0, size -1);
+	if (array == NULL || size < 2)
+		return;
+	lomuto_sort(array, size, 0, size - 1);
 }
-

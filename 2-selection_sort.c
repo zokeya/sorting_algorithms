ab4ba672022ec9630368129d13f6122366a4d5c3
zokeya <zdokeya@gmail.com>
2023-06-24 06:08:41 -0700

    Mandatory Tasks 24-06-2023

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,11 +7,11 @@
 */
 void swap_ints(int *a, int *b)
 {
-  int tmp;
+	int tmp;
 
-  tmp = *a;
-  *a = *b;
-  *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -24,26 +24,20 @@ void swap_ints(int *a, int *b)
 */
 void selection_sort(int *array, size_t size)
 {
-  int *min;
-  size_t j, k;
+	int *min;
+	size_t j, k;
 
-  if (array == NULL ||size < 2)
-  {
-    return;
-  }
-
-  for (j = 0; j < size - 1; j++)
-  {
-    min = array + j;
-    for (k = j + 1; k < size; k++)
-    {
-      min = (array[k] < *min) ? (array + k) : min;
-    }
-
-    if ((array + j) != min)
-    {
-      swap_ints(array + j, min);
-      print_array(array, size);
-    }
-  }
+	if (array == NULL || size < 2)
+		return;
+	for (j = 0; j < size - 1; j++)
+	{
+		min = array + j;
+		for (k = j + 1; k < size; k++)
+			min = (array[k] < *min) ? (array + k) : min;
+		if ((array + j) != min)
+		{
+			swap_ints(array + j, min);
+			print_array(array, size);
+		}
+	}
 }

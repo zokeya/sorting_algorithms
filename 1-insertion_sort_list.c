1f563d2a422a6c805a79a305214faf34a375271e
zokeya <zdokeya@gmail.com>
2023-06-26 11:32:24 +0300

    Advance Tasks 26-06-2023

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,19 +10,13 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
 	(*n1)->next = n2->next;
 	if (n2->next != NULL)
-	{
 		n2->next->prev = *n1;
-	}
 	n2->prev = (*n1)->prev;
 	n2->next = *n1;
 	if ((*n1)->prev != NULL)
-	{
 		(*n1)->prev->next = n2;
-	}
 	else
-	{
 		*h = n2;
-	}
 	(*n1)->prev = n2;
 	*n1 = n2->prev;
 }
@@ -39,9 +33,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-	{
 		return;
-	}
 	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
 		tmp = iter->next;
